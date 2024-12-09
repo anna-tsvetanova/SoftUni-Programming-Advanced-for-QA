8ceb0bf73254f7ae81c881675c12e6917ba58ae0
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-09 17:55:40 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/11.Towns/Program.js
+++ 14-JavaScript-Concepts-Exercise/11.Towns/Program.js
@@ -1,1 +1,19 @@
+function towns(data) {
+    let store = [];
+    for(let el of data) {
+        let [town, latitude, longitude] = el.split(" | ");
+        latitude = Number(latitude).toFixed(2);
+        longitude = Number(longitude).toFixed(2);
+        let obj = {
+            town, 
+            latitude,
+            longitude
+        };
 
+        store.push(obj);
+    }
+
+    for (let el of store) {
+        console.log(el)
+    }
+}
