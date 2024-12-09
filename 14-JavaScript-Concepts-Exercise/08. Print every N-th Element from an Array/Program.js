1e6793bda849fd6c2a69ef036494e3545f749fe2
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-09 16:44:40 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/08. Print every N-th Element from an Array/Program.js
+++ 14-JavaScript-Concepts-Exercise/08. Print every N-th Element from an Array/Program.js
@@ -1,1 +1,9 @@
+function printEveryNElement(arg) {
+    let data = arg.filter((el) => el !== ""); 
+    let step = Number(data[data.length - 1]);
 
+    for (let i=0; i < data.length - 1; i += step) {
+        console.log(data[i]);
+    }
+
+}
