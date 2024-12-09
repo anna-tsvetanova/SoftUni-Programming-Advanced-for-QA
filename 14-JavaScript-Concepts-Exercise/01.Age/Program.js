6eeead326a66233334b98fd1b2c02b986cd1ad6a
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-09 14:32:07 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/01.Age/Program.js
+++ 14-JavaScript-Concepts-Exercise/01.Age/Program.js
@@ -1,1 +1,31 @@
+function solve(data) {
+    let age = Number(data[0])
+
+    if (age < 0) {
+        console.log("out of bounds");
+      
+    } 
+    else if (age <= 2) {
+        console.log("baby");
+      
+    }
+    else if (age <= 13) {
+        console.log("child");
+
+    }
+    else if (age <= 19) {
+        console.log("teenager");
+      
+    } 
+    else if (age <= 65) {
+        console.log("adult");
+      
+    } 
+    else {
+        console.log("elder");
+    } 
+        
+}
+
+solve(14)
 
