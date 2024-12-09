663007a5c8c20ecb7fe92c7c351b0e97a64a56b5
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-09 17:39:29 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/09. Sorting Numbers/Program.js
+++ 14-JavaScript-Concepts-Exercise/09. Sorting Numbers/Program.js
@@ -1,1 +1,18 @@
+function sortingNumbers(data) {
+    let arr = data.map((el) => Number(el));
+    let result =[];
+    arr.sort((a,b) => a-b); 
+    let length = arr.length;
 
+    while(arr.length !==0) {
+        if (length % 2 !== 0 && arr.length ===1) {
+            result.push(arr.shift());
+        } else {
+            result.push(arr.shift());
+            result.push(arr.pop());
+        }
+    }
+    
+    return result.join("\n")
+   
+}
