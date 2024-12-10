c7e1ac9ab34f42bd100c9089375a522c6199a46d
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-10 14:07:26 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/12. Piccolo/Program.js
+++ 14-JavaScript-Concepts-Exercise/12. Piccolo/Program.js
@@ -1,1 +1,23 @@
+function piccolo(input) {
+    let parkingLot = new Set();
+
+    for (let entry of input) {
+        let [direction, carNumber] = entry.split(", ");
+
+        if (direction === "IN") {
+            parkingLot.add(carNumber);
+        } else if (direction === "OUT") {
+            parkingLot.delete(carNumber);
+        }
+    }
+
+    let sortedCars = Array.from(parkingLot).sort();
+
+    if (sortedCars.length === 0) {
+        console.log("Parking Lot is Empty");
+    } else {
+        console.log(sortedCars.join("\n"));
+    }
+}
+
 
