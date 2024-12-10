35b9db5d1f2acd8530669fccf5ee55bd7e824264
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-10 14:06:10 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/10. Employees/Program.js
+++ 14-JavaScript-Concepts-Exercise/10. Employees/Program.js
@@ -1,1 +1,12 @@
+function employees(names) {
+    let employeesList = {};
+
+    for (let name of names) {
+        employeesList[name] = name.length;
+    }
+
+    for (let employee in employeesList) {
+        console.log(`Name: ${employee} -- Personal Number: ${employeesList[employee]}`);
+    }
+}
 
