c6e65cc76b7ef6d246fd79289f807146897d8842
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-10 14:04:24 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/07. Array Rotation/Program.js
+++ 14-JavaScript-Concepts-Exercise/07. Array Rotation/Program.js
@@ -1,1 +1,10 @@
-
+function arrayRotation([arg1, arg2]) {
+    let myArr = arg1.substring(1,arg1.length-1).split(', ');
+    let times = parseInt(arg2);
+    for (let i = 0; i < times; i ++) {
+        let elem = myArr.shift();
+        myArr.push(elem);
+    }
+ 
+    console.log(myArr.join(' '));
+}
