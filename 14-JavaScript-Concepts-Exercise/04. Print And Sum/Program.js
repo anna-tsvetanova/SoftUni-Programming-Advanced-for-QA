184958037d31b6d71caf6e440f133da5d6035a72
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-10 14:00:00 +0200

    Update Program.js

--- 14-JavaScript-Concepts-Exercise/04. Print And Sum/Program.js
+++ 14-JavaScript-Concepts-Exercise/04. Print And Sum/Program.js
@@ -1,1 +1,13 @@
-
+function solve(data){
+  let start = Number(data[0]);
+  let end = Number(data[1]);
+    let sum = 0;
+    let nums = [];
+    for (let i = start; i <= end; i++){
+        sum += i;
+        nums.push(i);
+    }
+    let res = nums.join(" ");
+    console.log(res);
+    console.log(`Sum: ${sum}`);
+}
