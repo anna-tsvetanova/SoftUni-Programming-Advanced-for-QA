6b559389d03562daea5eb899d9cfb975583e1a89
Anna Tsvetanova <159261341+anna-tsvetanova@users.noreply.github.com>
2024-12-18 13:51:10 +0200

    Update DigitAndSymbolCounterTests.cs

--- 17.Exam-Preparation-II/02 - Digits-and-Symbol-Counter/DigitAndSymbolCounterTests.cs
+++ 17.Exam-Preparation-II/02 - Digits-and-Symbol-Counter/DigitAndSymbolCounterTests.cs
@@ -1,1 +1,101 @@
+using System;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
+namespace TestApp.Tests;
+
+public class DigitAndSymbolCounterTests
+{
+
+    [Test]
+    public void Test_EmptyStringInput_ReturnsEmptyDictionary()
+    {
+        //Arrange
+        string input = "";
+        Dictionary<string, int> expectedDictionary = new Dictionary<string, int>();
+
+        //Act
+        Dictionary<string,int>result = DigitAndSymbolCounter.CountDigitsAndSymbols(input);
+
+        //Assert
+        Assert.That(result, Is.EquivalentTo(expectedDictionary));
+    }
+
+    [Test]
+    public void Test_NoDigitsStringInput_ReturnsOnlyNonDigitsCount()
+    {
+        //Arrange
+        string input = "Hello World";
+        Dictionary<string, int> expectedDictionary = new Dictionary<string, int> 
+        {
+            {"non-digit symbol", 11 }
+        };
+
+        //Act
+        Dictionary<string, int> result = DigitAndSymbolCounter.CountDigitsAndSymbols(input);
+
+        //Assert
+        Assert.That(result, Is.EquivalentTo(expectedDictionary),"non-digit symbol -> 11 times" );
+    }
+
+    [Test]
+    public void Test_NoOddDigitsStringInput_ReturnsOnlyEvenDigitsAndNonDigitsCount()
+    {
+        //Arrange
+        string input = "2468abc";
+        Dictionary<string, int> expectedDictionary = new Dictionary<string, int>
+        {
+            {"even digit", 4 },
+            {"non-digit symbol", 3 }
+     
+        };
+
+        //Act
+        Dictionary<string, int> result = DigitAndSymbolCounter.CountDigitsAndSymbols(input);
+
+        //Assert
+        Assert.That(result, Is.EquivalentTo(expectedDictionary));
+    }
+
+    [Test]
+    public void Test_NoEvenDigitsStringInput_ReturnsOnlyOddDigitsAndNonDigitsCount()
+    {
+        //Arrange
+        string input = "QA is cool 579?";
+        Dictionary<string, int> expectedDictionary = new Dictionary<string, int>
+        {
+            {"odd digit", 3 },
+            {"non-digit symbol", 12 }
+
+        };
+
+        //Act
+        Dictionary<string, int> result = DigitAndSymbolCounter.CountDigitsAndSymbols(input);
+
+        //Assert
+        Assert.That(result, Is.EquivalentTo(expectedDictionary));
+    }
+
+    [Test]
+    public void Test_SymbolsEvenAndOddDigitsStringInput_ReturnsAllTypeOfCounts()
+    {
+        //Arrange
+        string input = "123abc!";
+        Dictionary<string, int> expectedDictionary = new Dictionary<string, int>
+        {
+            {"even digit", 1 },
+            {"odd digit", 2 },
+            {"non-digit symbol", 4 }
+
+        };
+
+        //Act
+        Dictionary<string, int> result = DigitAndSymbolCounter.CountDigitsAndSymbols(input);
+
+        //Assert
+        Assert.That(result, Is.EquivalentTo(expectedDictionary));
+    }
+}
